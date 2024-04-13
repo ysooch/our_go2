05f67846ab28db8611bfb9f77bbb1b4e91e89c77
Yeonsoo <changyeonsoo84@gmail.com>
2024-04-13 00:32:41 -0400

    new change

--- example/high_level/follow_sin.cpp
+++ example/high_level/follow_sin.cpp
@@ -1,6 +1,11 @@
 #include <unitree/robot/go2/sport/sport_client.hpp>
+#include <unitree/idl/go2/SportModeCmd_.hpp>
 #include <unitree/common/thread/recurrent_thread.hpp>
+#include <unitree/robot/go2/obstacles_avoid/obstacles_avoid_client.hpp>
 #include <math.h>
+#include <unistd.h>
+#include <iostream>
+#include <unitree/idl/ros2/PointCloud2_.hpp>
 
 class Custom
 {
@@ -13,6 +18,10 @@ public:
   int c = 0;
   float dt = 0.002; // 0.001~0.01
 };
+
+void printPosition(const std::array<float, 2>& position) {
+    std::cout << "Position - X: " << position[0] << ", Y: " << position[1] << std::endl;
+}
 
 void Custom::control()
 {
@@ -37,13 +46,14 @@ void Custom::control()
       p.vyaw = - M_PI * vx*2*0.6 * vx * M_PI * sin(M_PI * vx * var);
       path.push_back(p);
     }
+    std::cout << "Sending trajectory follow command, iteration: " << c << std::endl;
 
     ret = tc.TrajectoryFollow(path);
-    if(ret != 0){
-      std::cout << "Call TrajectoryFollow: " << ret << std::endl;
+    if(ret == 0){
+        std::cout << "TrajectoryFollow command was successful." << std::endl;
+    } else {
+        std::cout << "Call to TrajectoryFollow failed with error: " << ret << std::endl;
     }
-
-    std::cout << c << std::endl;
 }
 
 int main(int argc, char** argv)
@@ -63,3 +73,80 @@ int main(int argc, char** argv)
 
   return 0;
 }
+
+
+i/*nt main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " networkInterface" << std::endl;
+        return -1;
+    }
+    
+    unitree::robot::ChannelFactory::Instance()->Init(0, argv[1]);
+
+    // 스포츠 클라이언트 초기화
+    unitree::robot::go2::SportClient sport_client;
+    sport_client.SetTimeout(10.0f);
+    sport_client.Init();
+
+    const int totalDuration = 7; // 전체 지속 시간 15초
+
+    for(int i = 1; i <= totalDuration; ++i){
+        if(i <= 3) {
+            // 처음 5초 동안 전진
+            sport_client.Move(0.8, 0, 0);
+        } else if(i <= 2) {
+            // 다음 2초 동안 왼쪽으로 이동
+            sport_client.Move(0, 0.8, 0);
+        } else {
+            // 나머지 시간 동안 다시 전진
+            sport_client.Move(0.8, 0, 0);
+        }
+        sleep(1); // 1초 기다림
+    }
+
+    sport_client.StopMove(); // 이동 중지
+
+    return 0;
+}*/
+/*
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " networkInterface" << std::endl;
+        return -1;
+    }
+    
+    unitree::robot::ChannelFactory::Instance()->Init(0, argv[1]);
+
+    // Obstacles Avoid Client initialization and activation
+    unitree::robot::go2::ObstaclesAvoidClient obstacles_avoid_client;
+    obstacles_avoid_client.Init();
+    obstacles_avoid_client.SwitchSet(true);
+
+    // SportClient initialization
+    unitree::robot::go2::SportClient sport_client;
+    sport_client.SetTimeout(10.0f);
+    sport_client.Init();
+
+    bool obstacleDetected = false;
+    int duration = 15; // total duration time
+
+    for(int i = 0; i < duration; ++i){
+        obstacles_avoid_client.SwitchGet(obstacleDetected);
+        if(obstacleDetected){
+            // if obstacle is detected, move to left
+            sport_client.Move(0, 0.8, 0);
+        }else{
+            // else, move to forward
+            sport_client.Move(0.8, 0, 0);
+        }
+        sleep(1);
+    }
+
+    sport_client.StopMove();
+
+    return 0;
+}*/
